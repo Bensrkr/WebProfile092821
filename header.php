a0c2de74326a27e614782675b27cc0b327811db7
Bensrkr <melvincabanes25@gmail.com>
2021-09-27 18:48:25 -0700

    first commit

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en"><!-- -->
+<head>
+	<meta charset="utf-8">
+	<meta name = "viewport" 
+		  content="width=device-width, initial-scale=1"><!-- t0 be responseive in any devices -->
+	<title></title>
+
+<link rel="stylesheet" 
+	  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+	  integrity=">sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+	  crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" 
+		integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" 
+		crossorigin="anonymous"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+		integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+		crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" 
+		  type="text/css" 
+		  href="stylesheet.css?v=<?php echo time(); ?>">
+</head>
+
+<body>
+
+
+	<nav class="navbar navbar-inverse">	
+			<div class = "home container-fluid">
+					<div class="navbar-header">	
+						<button type="button" 
+								class="navbar-toggle" 
+								data-toggle = "collapse"
+								data-target = "#menu"> 
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" 
+						   href="index.php"><strong>BEN</strong></a>
+					</div> 
+
+			<div class="collapse navbar-collapse" id="menu" >								
+				<ul class="nav navbar-nav navbar-left">
+				<li class="	active"><a href="about.php">ABOUT</a></li>
+				<li class="	active"><a href="projects.php">PROJECTS</a></li>
+				<li class="	active"><a href="contact.php">CONTACT</a></li>	
+			</div>			
+	</nav>	
+
+	<div class="banner-img sm-6"></div>
+	</html>
